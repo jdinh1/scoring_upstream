1a1fb6275c157a44076cbf7b96b3a109bfd0b5b9
Jonathan Dinh <jdinh0608@yahoo.com>
2017-05-31 22:10:16 -0700

    Update scoring_test.php
    initial test script for score keeping in Upstream

--- scoring_test.php
+++ scoring_test.php
@@ -1,45 +1,113 @@
 <?php
 
+/*  Jonathan's Upstream's scoring system using SHA1 hashing between a shared secret key and a random generated token
+
+1.    Each time a request is made, a new token is generated and is sent back to the application.
+2.    The application will concatenate the new token with the secret key and produce a SHA1 hash of the string.
+3.    The string is sent back to the server via POST request
+4.    Server check for validity of the hash, and update the score based on the game mode (easy, med, hard)
+5.    Repeat step 1 until gameover is sent, session is destroyed then.
+
+ */
+
 // exit if no hash is seen
 if (!isset($_POST['extra'])) {
     exit();
 }
 
-$data = $_POST['extra'];
+session_start();
 
-// checking if shared key is correct
+// delete session if gameover
+if (isset($_POST['gameover']) && $_POST['gameover'] == 1) {
+    $msg = array(
+        "err" => "0",
+        "score" => isset($_SESSION['score']) ? $_SESSION['score'] : "0" ,
+        "token" => $_SESSION["token"],
+        "msg" => "gameover"
+    );
+    echo json_encode($msg);
+
+    session_unset();
+    session_destroy();
+    exit();
+}
+
+// Hash Validations
+$data = $_POST['extra'];
 $sharedSecretKey = "test1234";
 $sha1_sharedSecretKey = sha1($sharedSecretKey);
 
 if ($data === $sha1_sharedSecretKey) {
-echo "hi";
-    if (!isset($_SESSION["token"])) {
-        echo "hi2";
 
-        session_start();
-        #$_SESSION["token"] = random_str(20);
-        echo random_str(20);
-    } else {
-    echo "hi3";
-        if (isset($_POST['token']) && $_POST['token'] === sha1($_SESSION["token"])) {
+    if (!isset($_POST['token']) && !isset($_SESSION["token"])) {
+        $_SESSION['score'] = 0;
+        $_SESSION["token"] = generateRandomString(20);
+        $msg = array(
+            "err" => "0",
+            "score" => $_SESSION['score'],
+            "token" => $_SESSION["token"],
+            "msg" => "new session"
+        );
+    } else if (!isset($_POST['token'])) { // token not set
+        $msg = array(
+            "err" => "1",
+            "score" => isset($_SESSION['score']) ? $_SESSION['score'] : "0",
+            "token" => $_SESSION["token"],
+            "msg" => "invalid token"
+        );
+    } else if (isset($_POST['token']) && !isset($_SESSION["token"])) { // token set but there is no session
+        $_SESSION['score'] = 0;
+        $_SESSION["token"] = generateRandomString(20);
+        $msg = array(
+            "err" => "0",
+            "score" => $_SESSION['score'],
+            "token" => $_SESSION["token"],
+            "msg" => "invalid session"
+        );
+    } else if (isset($_POST['token']) && isset($_SESSION["token"]) && sha1($_SESSION["token"] . $sharedSecretKey) != $_POST['token'] ) { //token set but not matching with session token
+        $_SESSION['score'] = 0;
+        $_SESSION["token"] = generateRandomString(20);
+        $msg = array(
+            "err" => "1",
+            "score" => $_SESSION['score'],
+            "token" => $_SESSION["token"],
+            "msg" => "invalid token"
+        );
+    } else if (isset($_POST['token']) && $_POST['token'] == sha1($_SESSION["token"] . $sharedSecretKey) && isset($_POST['mode']) && $_POST['mode'] != 0 ){ // everything is set correctly and tokens are matching
+        // generate new token to prevent relay attack
+        $_SESSION["token"] = generateRandomString(20);
 
-
+        // scoring based on mode
+        switch($_POST['mode']) {
+            case 1:
+                $_SESSION['score'] += 10;
+                break;
+            case 2:
+                $_SESSION['score'] += 20;
+                break;
+            case 3:
+                $_SESSION['score'] += 30;
+                break;
         }
 
+        $msg = array(
+            "err" => "0",
+            "score" => $_SESSION['score'],
+            "token" => $_SESSION["token"],
+            "msg" => "score updated"
+        );
     }
-
-
 }
 
-function random_str($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-{
-    $str = '';
-    $max = mb_strlen($keyspace, '8bit') - 1;
-    for ($i = 0; $i < $length; ++$i) {
-        $str .= $keyspace[random_int(0, $max)];
-    }
-    return $str;
+if (isset($msg)) {
+    echo json_encode($msg);
+    exit();
 }
+
+function generateRandomString($length = 15) {
+    return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+}
+
 
 
 ?>
